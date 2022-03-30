14abb30d502dfdfefdfe1187cd8cefeea70f6f12
enken0609 <mkmky42487@gmail.com>
2022-03-31 05:52:10 +0900

    ナビバーにレース追加

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -45,6 +45,11 @@ export const Navbar = () => {
           }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}
         >
           <div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto'>
+            <Link href='/race/hotakaskimosummitchallenge'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white text-sm font-bold items-center justify-center mr-4 hover:bg-gray-800 hover:text-white '>
+                レース
+              </a>
+            </Link>
             <Link href='/events'>
               <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white text-sm font-bold items-center justify-center mr-4 hover:bg-gray-800 hover:text-white '>
                 イベント
